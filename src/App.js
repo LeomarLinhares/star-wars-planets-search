1cb472b2bdcae70905b545e472a05b62424fcf9d
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-04 17:15:22 -0300

    adds Header component to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 import Provider from './Context/GlobalProvider';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Provider>
+      <Header />
       <Home />
     </Provider>
   );
