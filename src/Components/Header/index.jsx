01b31ac66aa1dde7df690cb902a99750b8ecd09c
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-04 17:54:34 -0300

    creates interaction functionality between name input and the state responsible for search filtering

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import GlobalContext from '../../Context/GlobalContext';
 
 export default function Header() {
+  const { filter, setFilter } = useContext(GlobalContext);
+  const { filters: { filterByName: { name } } } = filter;
+  const handleFilter = ({ target: { value } }) => {
+    const { filters } = filter;
+    setFilter({ filters: { ...filters, filterByName: { name: value } } });
+  };
+
   return (
     <header>
-      <input type="text" />
+      <input
+        type="text"
+        data-testid="name-filter"
+        value={ name }
+        onChange={ handleFilter }
+      />
     </header>
   );
 }
