7a4db40e1842818b661c9cc74e1e09cc3c87bef5
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-04 16:25:43 -0300

    creates the Home page to handle the consumption of the Context and handle the keys treatment

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import Table from '../../Components/Table';
+import GlobalContext from '../../Context/GlobalContext';
+import treatKeys from '../../helpers/treatKeys';
+
+export default function Home() {
+  const { data, loading } = useContext(GlobalContext);
+
+  return (
+    <main>
+      {
+        loading
+          ? 'Loading, wait...'
+          : <Table data={ data } headerLine={ treatKeys(data) } />
+      }
+    </main>
+  );
+}
