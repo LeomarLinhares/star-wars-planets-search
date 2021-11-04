48c4b202d56b66c24e6b6e1559de06715db33f41
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-04 16:13:07 -0300

    removes responsibility for the fetch from App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import Table from './Components/Table';
-import GlobalContext from './Context/GlobalContext';
+import Provider from './Context/GlobalProvider';
+import Home from './pages/Home';
 
 function App() {
-  const { planets, setPlanets, loading, setLoading } = useContext(GlobalContext);
-
-  useEffect(() => {
-    const fetchAPI = async () => {
-      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/?format=json');
-      const data = await response.json();
-      setPlanets(data.results.map((result) => {
-        delete result.residents;
-        return result;
-      }));
-      setLoading(false);
-    };
-    fetchAPI();
-  }, []);
-
   return (
-    <main>
-      {
-        loading
-          ? ''
-          : <Table planets={ planets } />
-      }
-    </main>
+    <Provider>
+      <Home />
+    </Provider>
   );
 }
 
