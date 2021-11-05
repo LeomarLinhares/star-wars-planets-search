ab4128b4fe3193ca9479337b37ba28819316cb96
Leomar Linhares <leomar_linhares@hotmail.com>
2021-11-05 10:22:42 -0300

    fixes post-renaming import of treatKeys to stringTreatment

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import Table from '../../Components/Table';
 import GlobalContext from '../../Context/GlobalContext';
-import treatKeys from '../../helpers/treatKeys';
+import { treatKeys } from '../../helpers/stringTreatment';
 
 export default function Home() {
   const { data, loading } = useContext(GlobalContext);
